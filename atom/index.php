e9f34981b2a9406b87d2418d240f56aa0f326d37
Simon Barnes <simonbarnes1@googlemail.com>
2012-08-08 13:13:53 +0100

    Added twitter feed counter

--- atom/index.php
+++ atom/index.php
@@ -1,12 +1,26 @@
+<meta http-equiv="refresh" content="5" />
 <?PHP
+ date_default_timezone_set('UTC');
   include "class.myatomparser.php";
 
-  # where is the feed located?
-  $url = "http://search.twitter.com/search.atom?q=yrs2012&rpp=100";
-
-  # create object to hold data and display output
-  $atom_parser = new myAtomParser($url);
-
-  $output = $atom_parser->getOutput();	# returns string containing HTML
-  echo $output;
+  $twitter_hash=$_REQUEST["twit"];
+  $amount=$_REQUEST["amt"];
+  $atom_parser = new myAtomParser("http://search.twitter.com/search.atom?q=$twitter_hash&rpp=100");
+  $raw = $atom_parser->getRawOutput();
+  $count=0;
+ foreach ($raw["FEED"]["ENTRY"] as $entry) {
+    $timestamp=strtotime($entry["PUBLISHED"]);
+    if ($timestamp <= strtotime('-2 minutes')) {
+    }
+    else{;
+    $count=$count+1;}
+    }
+$url = "http://search.twitter.com/search.atom?q=$twitter_hash&rpp=$count";
+$atom = new myAtomParser($url);
+  $output = $atom->getOutput();
+  if ($count>0){
+  echo $output.PHP_EOL.PHP_EOL.PHP_EOL;
+  print_r($atom->getRawOutput());
+  }else{
+  echo "no feeds in the last 5 minutes";}
 ?>
