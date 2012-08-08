31861c15b1bdc3e487752dce2ffe10eec43e4630
Simon Barnes <simonbarnes1@googlemail.com>
2012-08-08 14:39:52 +0100

    Turned into a class, added countChar function and cleaned code

--- atom/index.php
+++ atom/index.php
@@ -16,29 +16,43 @@
 	</head>
 	<body>
 <?PHP
- date_default_timezone_set('UTC');
+class twitTone {
+  function __construct($hash) {
   include "class.myatomparser.php";
-
-  $twitter_hash=$_GET["twit"];
-  $amount=$_GET["amt"];
-  $atom_parser = new myAtomParser("http://search.twitter.com/search.atom?q=$twitter_hash&rpp=100");
-  $raw = $atom_parser->getRawOutput();
+  date_default_timezone_set('UTC');
+  $this->twitter_hash=$hash;
+  $this->debug=0;
+  }
+  private function getFeeds($time) {
+  $t_hash=$this->twitter_hash;
+  $data=new myAtomParser("http://search.twitter.com/search.atom?q=$t_hash&rpp=100");
+  $raw=$data->getRawOutput();
   $count=0;
- foreach ($raw["FEED"]["ENTRY"] as $entry) {
-    $timestamp=strtotime($entry["PUBLISHED"]);
-    if ($timestamp <= strtotime('-5 minutes')) {
-    }
-    else{;
-    $count=$count+1;}
-    }
-$url = "http://search.twitter.com/search.atom?q=$twitter_hash&rpp=$count";
-$atom = new myAtomParser($url);
-  $output = $atom->getOutput();
-  if ($count>0){
-  echo $output.PHP_EOL.PHP_EOL.PHP_EOL;
-  //print_r($atom->getRawOutput());
-  }else{
-  echo "no feeds in the last 5 minutes";}
+  foreach ($raw["FEED"]["ENTRY"] as $entry){$ts=strtotime($entry["PUBLISHED"]);if($ts<=strtotime($time)){}else{$count=$count+1;}}
+  if ($this->debug>0){echo $count."<br>";}
+  return $count;
+  }
+  
+  public function returnTweets() {
+  $count=$this->getFeeds("-5 minutes");
+  $url="http://search.twitter.com/search.atom?q=".$this->twitter_hash."&rpp=".$count;
+  if ($this->debug>0){echo $url."<br>";}
+  $this->atom=new myAtomParser($url);
+  $output = $this->atom->getOutput();
+  if ($count>0){return $output;}else{return "no feeds in the last 5 minutes";}
+  }
+  
+  public function countChars() {
+  if (!$this->atom){$this->returnTweets();}
+  $feeds=$this->atom->getRawOutput();
+  foreach ($feeds["FEED"]["ENTRY"] as $entry) {$all=$all.$entry["CONTENT"];}
+  return strlen($all);
+  }
+}
+ $tt=new twitTone($_GET["twit"]);
+ //$tt->debug=1;
+ echo $tt->returnTweets();
+ echo $tt->countChars();
 ?>
 	</body>
 </html>
