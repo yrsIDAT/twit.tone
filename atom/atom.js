bc6e7602e2c61b4f9eb92d8f49bea7f49387cc7e
Simon Barnes <simonbarnes1@googlemail.com>
2012-08-10 13:12:48 +0100

    added Auto mode and various other functions

--- atom/atom.js
+++ atom/atom.js
@@ -3,7 +3,7 @@ to use:
 initialise connection with load() reccommended putting in <body onload="load()" ....
 twitTone function:
  ('twitter feed (string OR array)', 'php twitTone function(string OR array)', 'function to return the response', 'array of key:value to be sent to php')
-ignore function wait() unless debugging
+ignore function recieve() unless debugging
 
 */
 function load() {//load new http object on body onload
@@ -11,7 +11,9 @@ if (window.XMLHttpRequest){xmlhttp=new XMLHttpRequest();}
 else{xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");}}
 
 function twitTone(hash, func, callback, dict) {//send request to php
-resp=function() {wait(this, callback);}
+var img=document.getElementById("loading").style;
+img.display="block";img.textAlign="center";
+resp=function() {recieve(this, callback);}
 xmlhttp.onreadystatechange=function(){if (xmlhttp.readyState==4 && xmlhttp.status==200){
 resp.call(xmlhttp.responseText);
 }}
@@ -20,14 +22,11 @@ xmlhttp.open("GET","atom/response.php?twit="+hash+"&func="+func+extra,true);
 xmlhttp.send();
 }
 
-function wait(data, callback){//wait for response and push to array
-var img=document.getElementById("loading").style;
-if (data==""){img.display="block";setTimeout(function(){wait(data, callback);}, 10)}//if no response
-else {//once a response is recieved
+function recieve(data, callback){//recieve response and push to array
 var arr=new Array();
-img.display="none";
+document.getElementById("loading").style.display="none";
 var newval=data.split(",");
 data="";
 for (var i=1;i<=newval.length-1;i++) {
 arr.push(newval[i-1]);
-}callback.call(arr)}}//close function
+}callback.call(arr)}//close function
